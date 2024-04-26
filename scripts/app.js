42cb4eece6ee9d6801d9f1776e29635f5f1ef9f7
Luqman B <luqmanmbutt.dev@gmail.com>
2024-04-26 18:51:41 +0100

    got the sound working by clicking each button by writing function for each div

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,74 @@
 
+const audioPlayer = document.querySelector('audio')
+
+// const workIt = document.querySelector('#work_it')
+// const makeIt = document.querySelector('#make_it')
+
+
+
+
+// const playWorkIt = () => {
+   
+//     audioPlayer.src = './sounds/work_it.wav'
+
+//     audioPlayer.play()
+// }
+
+// const playMakeIt = () => {
+   
+//     audioPlayer.src = './sounds/make_it.wav'
+
+//     audioPlayer.play()
+// }
+
+// workIt.addEventListener('click', playWorkIt)
+// makeIt.addEventListener('click', playMakeIt)
+
+
+const daftSound = [
+    {IdObject: "#work_it", soundToPlay: "./sounds/work_it.wav"},
+    {IdObject: "#make_it", soundToPlay: "./sounds/make_it.wav"},
+    {IdObject: "#do_it", soundToPlay: "./sounds/do_it.wav"},
+    {IdObject: "#makes_us", soundToPlay: "./sounds/makes_us.wav"},
+    {IdObject: "#harder", soundToPlay: "./sounds/harder.wav"},
+    {IdObject: "#better", soundToPlay: "./sounds/better.wav"},
+    {IdObject: "#faster", soundToPlay: "./sounds/faster.wav"},
+    {IdObject: "#stronger", soundToPlay: "./sounds/stronger.wav"},
+    {IdObject: "#more_than", soundToPlay: "./sounds/more_than.wav"},
+    {IdObject: "#hour", soundToPlay: "./sounds/hour.wav"},
+    {IdObject: "#our", soundToPlay: "./sounds/our.wav"},
+    {IdObject: "#never", soundToPlay: "./sounds/never.wav"},
+    {IdObject: "#ever", soundToPlay: "./sounds/ever.wav"},
+    {IdObject: "#after", soundToPlay: "./sounds/after.wav"},
+    {IdObject: "#work_is", soundToPlay: "./sounds/work_is.wav"},
+    {IdObject: "#over", soundToPlay: "./sounds/over.wav"}
+]
+
+myButton = document.querySelector('#player')
+
+myButton.addEventListener('click', arrayFunction)
+
+
+
+function arrayFunction(event) {
+    daftSound.forEach((index) => {
+        if(myButton === daftSound.idObject) {
+            console.log("HELLO")
+            audioPlayer.src = daftSound.soundToPlay
+            audioPlayer.play()    
+        
+    console.log(event.target.id)
+        }
+            console.log(event.target.id);
+        })
+}
+
+
+
+
+
+
+
 /*-------------------------------- Constants --------------------------------*/
 
 
